25ae95d6960579f3899b6c5e047be2bdc6611788
Eunji <eeunjiilee@gmail.com>
2023-11-21 19:10:38 +0900

    refactor: early return pattern

--- src/components/ProductDetail/PriceHistory/CustomTooltip.tsx
+++ src/components/ProductDetail/PriceHistory/CustomTooltip.tsx
@@ -8,39 +8,40 @@ const CustomTooltip: React.FC<TooltipProps<number, string>> = ({
   active,
   payload,
 }) => {
-  if (active && payload && payload.length) {
-    const { price, createdAt, diff } = payload[0].payload;
-    const title = `
+  // early return
+  if (!active || !payload || !payload.length) {
+    return null;
+  }
+
+  const { price, createdAt, diff } = payload[0].payload;
+  const title = `
       ${createdAt.substring(4, 6)}월 
       ${createdAt.substring(6, 8)}일 
       가격 정보
     `;
-    const priceText = `${price.toLocaleString()}원`;
+  const priceText = `${price.toLocaleString()}원`;
 
-    let diffText = '';
-    let diffType: DiffType = 'same';
-    if (diff > 0) {
-      diffText = `▲ ${diff.toLocaleString()}원`;
-      diffType = 'increment';
-    } else if (diff < 0) {
-      diffText = `▼ ${(-diff).toLocaleString()}원`;
-      diffType = 'decrement';
-    } else {
-      diffText = '( -- )';
-    }
-
-    return (
-      <div css={styles.customTooltip}>
-        <h3 css={styles.tooltipTitle}>{title}</h3>
-        <div css={styles.tooltipContent}>
-          <div css={styles.price}>{priceText}</div>
-          <div css={styles.priceInfo[diffType]}>{diffText}</div>
-        </div>
-      </div>
-    );
+  let diffText = '';
+  let diffType: DiffType = 'same';
+  if (diff > 0) {
+    diffText = `▲ ${diff.toLocaleString()}원`;
+    diffType = 'increment';
+  } else if (diff < 0) {
+    diffText = `▼ ${(-diff).toLocaleString()}원`;
+    diffType = 'decrement';
   } else {
-    return null;
+    diffText = '( -- )';
   }
+
+  return (
+    <div css={styles.customTooltip}>
+      <h3 css={styles.tooltipTitle}>{title}</h3>
+      <div css={styles.tooltipContent}>
+        <div css={styles.price}>{priceText}</div>
+        <div css={styles.priceInfo[diffType]}>{diffText}</div>
+      </div>
+    </div>
+  );
 };
 
 export default CustomTooltip;
