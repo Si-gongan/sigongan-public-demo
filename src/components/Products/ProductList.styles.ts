4e3e308325323a55b94fa3fb44eee2ac39cb9770
Eunji <eeunjiilee@gmail.com>
2024-02-13 16:24:01 +0900

    fix: focus after clicking load more button

---
+++ src/components/Products/ProductList.styles.ts
@@ -0,0 +1,7 @@
+import { css } from '@emotion/react';
+
+export const ulStyle = css`
+  display: flex;
+  flex-wrap: wrap;
+  margin: -8px;
+`;
---
+++ src/components/Products/ProductList.tsx
@@ -0,0 +1,39 @@
+/** @jsxImportSource @emotion/react */
+
+import { useEffect, useRef } from 'react';
+import { InfiniteData } from '@tanstack/react-query';
+import Product from './Product';
+import { ulStyle } from './ProductList.styles';
+import { ProductsResponseModel } from '../../api/axios/coupang/types';
+
+interface Props {
+  data: InfiniteData<ProductsResponseModel, unknown> | undefined;
+}
+
+function ProductList({ data }: Props) {
+  const focusRef = useRef<HTMLAnchorElement>(null);
+
+  useEffect(() => {
+    focusRef?.current?.focus();
+  }, [data?.pages.length]);
+
+  return (
+    <ul css={ulStyle}>
+      {data?.pages?.map((page, idx) => {
+        const lastIdx = data.pages.length - 1;
+        const isLastPage = idx === lastIdx;
+        return page.products.map((product, i) => {
+          return (
+            <Product
+              key={product.id}
+              product={product}
+              focusRef={isLastPage && i === 0 ? focusRef : undefined}
+            />
+          );
+        });
+      })}
+    </ul>
+  );
+}
+
+export default ProductList;
